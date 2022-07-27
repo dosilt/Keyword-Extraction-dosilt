65296fe44634786fc89ab5351edb7b3b8dafce41
Jaejin Seo <37330349+dosilt@users.noreply.github.com>
2022-07-28 01:19:58 +0900

    Update dataset.py

--- 4. Keyword_DP/dataset.py
+++ 4. Keyword_DP/dataset.py
@@ -21,13 +21,5 @@ def special_token_remover(x):
 
 
 def load_data(args):
-    # if 'csv' in args.data_path:
-    #     df = pd.read_csv(args.data_path, usecols=['본문']).dropna()
-    #     data = df['본문'].apply(lambda x:special_token_remover(x)).values
-    # else:
-    #     with open(args.data_path, 'r', encoding='utf-8') as f:
-    #         datas = [x.strip() for x in f.readlines()]
-    #     data = [special_token_remover(x) for x in datas]
-
     data = ["텍스트 요약은 텍스트의 관련 정보를 나타내는 여러 가지 방법으로 구성된 광범위한 항목입니다. 이 설명서에 설명된 문서 요약 기능을 통해 추출 텍스트 요약을 사용하여 문서의 요약을 생성할 수 있습니다. 원본 콘텐츠 내에서 가장 중요하거나 관련성 있는 정보를 집합적으로 나타내는 문장을 추출합니다. 이 기능은 너무 길어서 읽을 수 없다고 생각할 수 있는 콘텐츠를 줄이도록 설계되었습니다. 예를 들어 문서, 논문 또는 문서를 주요 문장으로 압축할 수 있습니다."]
     return data
